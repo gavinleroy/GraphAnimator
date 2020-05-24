b894ca947625953c08aacb4b299537a49a6d0840
gavinleroy <gavinleroy6@gmail.com>
2020-05-23 22:06:52 -0500

    Increased efficiency of Kruskals algorithm when picking next lightest edge

--- src/DynamicGraph/Edge.java
+++ src/DynamicGraph/Edge.java
@@ -2,13 +2,13 @@ package DynamicGraph;
 
 import java.util.*;
 
-public class Edge{
+public class Edge implements Comparable<Edge>{
 	public boolean isWeighted;
 	public boolean isDirected;
-	public int Weight;
-	public int Color;
-	public int Begin;
-	public int End;
+	public Integer Weight;
+	public Integer Color;
+	public Integer Begin;
+	public Integer End;
 	public Edge(boolean _w, boolean _d, int w, int c, int p1, int p2){
 		this.isWeighted = _w;
 		this.isDirected = _d;
@@ -16,5 +16,9 @@ public class Edge{
 		this.Color = c;
 		this.Begin = p1;
 		this.End = p2;
+	}
+	@Override 
+	public int compareTo(Edge e){
+		return this.Weight.compareTo(e.Weight);
 	}
 }
--- src/DynamicGraph/KruskalsAlgorithm.java
+++ src/DynamicGraph/KruskalsAlgorithm.java
@@ -6,17 +6,17 @@ public class KruskalsAlgorithm implements Algorithm{
 
 	private Graph graph;
 	private boolean done;
-	private boolean used[];
 	private int ind; // Index of the sorted edges array
 	private int parent[];
+	private Edge sorted_edges[];
 
 	public KruskalsAlgorithm(Graph g){
 		parent = new int[g.V];
+		sorted_edges = g.Edges;
+		Arrays.sort(sorted_edges); // Sort edges
 		for(int i = 0; i < g.V; i++) parent[i]=-1;
-		used = new boolean[g.E];
 		graph = g;
 		ind = 0;
-		// Sort edges
 	}
 
 	private int find(int p){
@@ -36,44 +36,24 @@ public class KruskalsAlgorithm implements Algorithm{
 		return true;
 	}
 
-	private boolean focus(int u, int v){
-		//TODO: implement
-		// Should focus both U and V along with its edge
-		return false;
-	}
-
-	private int findSmallest(){
-		int minI = -1, min = Integer.MAX_VALUE;
-		for(int i = 0; i < graph.Edges.length; i++){
-			if(graph.Edges[i].Weight < min && !used[i]){
-				minI = i;
-				min = graph.Edges[i].Weight;
-			}
-		}
-		return minI;
-	}
-
 	public void step(){
-		// 1) Pick next smallest edge
-		int i = findSmallest();
-		if(i == -1){ 
+		if(done) return;
+		int i = ind++;
+		if(i >= graph.E){ 
 			done = true;
 			return;
 		}
-
-		Edge e = graph.Edges[i];
-		e.Color = Colors.EHIGHLIGHTED;
+		Edge e = sorted_edges[i];
 		// 2) If it doesn't form a cycle then highlight it
 		if(join(e.Begin, e.End)){
 			// Focus Edge
 			e.Color = Colors.EDONE;
+			graph.Verticies[e.Begin].Color = Colors.VDONE;
+			graph.Verticies[e.End].Color = Colors.VDONE;
 			// Join the verticies 
 		}else{
 			e.Color = Colors.EFOCUSED;
 		}
-		used[i] = true; // Say that it is used
-
-		System.out.println(parent);
 	}
 
 	public boolean isDone(){
