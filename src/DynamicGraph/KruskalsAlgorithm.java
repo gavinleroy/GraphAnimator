ae8214e611e2572ae8dcf0caa6614717d8b03cf6
gavinleroy <gavinleroy6@gmail.com>
2020-05-24 22:25:24 -0500

    Added commentary to Step method

--- src/DynamicGraph/KruskalsAlgorithm.java
+++ src/DynamicGraph/KruskalsAlgorithm.java
@@ -25,6 +25,11 @@ public class KruskalsAlgorithm implements Algorithm{
 		else return (parent[p] = find(parent[p]));
 	}
 
+	/*
+	 * Simple implementation of Disjoint Set Union, if p1 and p2 belong to 
+	 * seperate disjoint sets then we combine them and return true, otherwise
+	 * we cannot and return false.
+	 * */
 	private boolean join(int p1, int p2){
 		int u = find(p1);
 		int v = find(p2);
@@ -37,6 +42,10 @@ public class KruskalsAlgorithm implements Algorithm{
 		return true;
 	}
 
+	/*
+	 * Take one step of the algorithm, for Kruskals algorithm
+	 * this involves analyzing the next lightest edge in the graph.
+	 * */
 	public void step(){
 		if(done) return;
 		int i = ind++;
@@ -46,17 +55,21 @@ public class KruskalsAlgorithm implements Algorithm{
 		}
 		Edge e = sorted_edges[i];
 		// 2) If it doesn't form a cycle then highlight it
-		if(join(e.Begin, e.End)){
+		if(join(e.Begin, e.End)){ // Add edge
 			// Focus Edge
 			e.Color = Colors.EDONE;
 			graph.Verticies[e.Begin].Color = Colors.VDONE;
 			graph.Verticies[e.End].Color = Colors.VDONE;
 			added++;
 			// Join the verticies 
-		}else{
+		}else{ // Edge cannot be added because it will create cycle
 			e.Color = Colors.EFOCUSED;
-			if(added == graph.V-1){
+			// If number of added edges is V-1 
+			// then we have connected all verticies 
+			// and can indicate that we are done
+			if(added == graph.V-1){ 
 				done = true;
+				// Gray out all edges
 				while(ind<graph.E) sorted_edges[ind++].Color=Colors.EFOCUSED;
 			}
 		}
